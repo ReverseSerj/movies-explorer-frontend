37b1e76855773ea709f1f027ad02fb3536a84fb1
ReverseSerj <sej.petrov@gmail.com>
2023-08-31 13:41:00 +0300

    fixes
    fix background 404 and adaptive resolution layout component 'Profile'

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -5,12 +5,14 @@ export default function PageNotFound() {
   const navigate = useNavigate();
   
   return (
-    <section className='not-found'>
-      <div className='not-found__error-container'>
-        <h1 className='not-found__error'>404</h1>
-        <p className='not-found__error-text'>Страница не найдена</p>
-      </div>
-      <button className='not-found__button' onClick={() => navigate(-1)}>Назад</button>
-    </section>
+    <main>
+      <section className='not-found'>
+        <div className='not-found__error-container'>
+          <h1 className='not-found__error'>404</h1>
+          <p className='not-found__error-text'>Страница не найдена</p>
+        </div>
+        <button className='not-found__button' onClick={() => navigate(-1)}>Назад</button>
+      </section>
+    </main>
   );
 };
